6a766cf97aa9419300c4579a46c3b326aa7be808
Achyuth Varma <penmetcha.s@northeastern.edu>
2023-03-30 22:49:14 -0400

    Changed logical condition to check quantity 💀

--- src/middleware/CheckQuantityNumber.js
+++ src/middleware/CheckQuantityNumber.js
@@ -4,7 +4,9 @@ import { logger } from "../winston-log/winston.js";
 const checkQuantityNumber = (request, response, next) => {
   const { quantity } = request.body;
 
-  if (quantity !== undefined || quantity !== null) {
+  console.log("Quantity: " + quantity);
+
+  if (quantity !== undefined && quantity !== null) {
     if (!Number.isInteger(quantity)) {
       logger.error("User didn't give the quantity as integer");
       throw new BadRequestException("Given Quantity is not an integer");
